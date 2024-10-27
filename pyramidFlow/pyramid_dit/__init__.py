d3495b0b25c2939b420715bdec05e863ed8c4967
deboradum <pvwpvw@protonmail.com>
2024-10-27 07:43:14 +0100

    remove dir

--- pyramidFlow/pyramid_dit/__init__.py
+++
@@ -1,3 +0,0 @@
-from .modeling_pyramid_mmdit import PyramidDiffusionMMDiT
-from .pyramid_dit_for_video_gen_pipeline import PyramidDiTForVideoGeneration
-from .modeling_text_encoder import SD3TextEncoderWithMask
